3514f81f541c398a5ea7dd8012e8c3cdd72ade76
Byron Luk <byronluk@gmail.com>
2018-01-18 18:02:22 -0800

    added state, integrated handwrite api

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,75 @@ import React, { Component } from 'react';
 import { HandWrite, SpellCheck } from './Input';
 import Output from './Output';
 
+const handwritingToken = 'TGWVH0Z1JA9A93J9:849SKSQTRNP52HA8';
+const encoded = btoa(handwritingToken);
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      inputText: '',
+      imgURL: '',
     };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onHandwriteClick = this.onHandwriteClick.bind(this);
+  }
+
+  onInputChange(e) {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [[name]]: value,
+    });
+    // const params = {
+    //   handwriting_id: '5WGWVYD800X0',
+    //   text: this.state.inputText,
+    // };
+    // const esc = encodeURIComponent;
+    // const query = Object.keys(params).map(key => `${esc(key)}=${esc(params[key])}`).join('&');
+    // //  later on... add option for pdf(download) or png(view)
+    // const url = `https://api.handwriting.io/render/png?${query}`;
+    // fetch(url, {
+    //   method: 'GET',
+    //   headers: {
+    //     Authorization: `Basic ${encoded}`,
+    //   } })
+    //     .then((response) => {
+    //       this.setState({
+    //         imgURL: response.url,
+    //       });
+    //     })
+    //     .catch((error) => {
+    //       console.error(error);
+    //     });
+  }
+
+  onHandwriteClick(e) {
+    e.preventDefault();
+    //  creates query parameters for url
+    const params = {
+      handwriting_id: '5WGWVYD800X0',
+      text: this.state.inputText,
+    };
+    const esc = encodeURIComponent;
+    const query = Object.keys(params).map(key => `${esc(key)}=${esc(params[key])}`).join('&');
+    //  later on... add option for pdf(download) or png(view)
+    const url = `https://api.handwriting.io/render/png?${query}`;
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Basic ${encoded}`,
+      } })
+        .then((response) => {
+          this.setState({
+            imgURL: response.url,
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
   }
 
   render() {
@@ -15,7 +78,11 @@ class App extends Component {
       <div className='App'>
         <h1>Write a letter</h1>
         <div className='input-div'>
-          <HandWrite />
+          <img src={ this.state.imgURL } alt='handwritten letter' />
+          <HandWrite
+            onInputChange={ this.onInputChange }
+            onHandwriteClick={ this.onHandwriteClick }
+          />
           <SpellCheck />
         </div>
         <Output />
--- src/Input.jsx
+++ src/Input.jsx
@@ -5,11 +5,11 @@ class HandWrite extends Component {
   render() {
     return (
       <form className='handwrite-form'>
-        <textarea className='input-text' />
+        <textarea className='input-text' name='inputText' onChange={ this.props.onInputChange } />
         <button className='spellcheck-button'>
           SpellCheck
         </button>
-        <button className='handwrite-button'>
+        <button className='handwrite-button' onClick={ this.props.onHandwriteClick }>
           Handwrite
         </button>
       </form>
@@ -22,7 +22,7 @@ class SpellCheck extends Component {
   render() {
     return (
       <form className='spellcheck-form'>
-        <textarea className='spellcheck-text' />
+        <textarea className='spellcheck-text' name='spellcheckText' />
         <button className='reject-button'>
           Reject
         </button>
